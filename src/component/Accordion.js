889de917f2ace3aa10f0f8d6f5df3c7644689303
unknown <leonardohchoi@gmail.com>
2023-06-20 12:16:11 -0300

    add icons to accordion

--- src/component/Accordion.js
+++ src/component/Accordion.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
 
 function Accordion({ items }) {
   const [expandedIndex, setExpandedIndex] = useState(2);
@@ -11,7 +12,9 @@ function Accordion({ items }) {
     const isExpanded = index === expandedIndex;
     console.log(isExpanded);
 
-    const icon = <span>{isExpanded ? "DOWN" : "LEFT"}</span>;
+    const icon = (
+      <span>{isExpanded ? <GoChevronDown /> : <GoChevronLeft />}</span>
+    );
 
     return (
       <div key={item.id}>
