6c0d812f5d15775a996847af296aa10c5a408da2
unknown <leonardohchoi@gmail.com>
2023-06-20 19:10:49 -0300

    change click handler

--- src/component/Accordion.js
+++ src/component/Accordion.js
@@ -2,10 +2,14 @@ import React, { useState } from "react";
 import { GoChevronDown, GoChevronLeft } from "react-icons/go";
 
 function Accordion({ items }) {
-  const [expandedIndex, setExpandedIndex] = useState(2);
+  const [expandedIndex, setExpandedIndex] = useState(-1);
 
   const handleClick = (nextIndex) => {
-    setExpandedIndex(nextIndex);
+    if (nextIndex === expandedIndex) {
+      setExpandedIndex(-1);
+    } else {
+      setExpandedIndex(nextIndex);
+    }
   };
 
   const renderedItems = items.map((item, index) => {
@@ -13,16 +17,21 @@ function Accordion({ items }) {
     console.log(isExpanded);
 
     const icon = (
-      <span>{isExpanded ? <GoChevronDown /> : <GoChevronLeft />}</span>
+      <span className="text-2xl">
+        {isExpanded ? <GoChevronDown /> : <GoChevronLeft />}
+      </span>
     );
 
     return (
       <div key={item.id}>
-        <div onClick={() => handleClick(index)}>
+        <div
+          className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer"
+          onClick={() => handleClick(index)}
+        >
           {item.label}
           {icon}
         </div>
-        {isExpanded && <div>{item.content}</div>}
+        {isExpanded && <div className="border-b p-5">{item.content}</div>}
       </div>
     );
   });
