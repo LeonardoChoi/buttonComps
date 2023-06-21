6fdddf75e7d57466059f45cbda9f46fb3ab819f4
Leo Choi <leonardohchoi@gmail.com>
2023-06-21 12:30:45 -0300

    create dropdown component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
-import Accordion from "./component/Accordion";
+import Dropdown from "./component/Dropdown";
 
 function App() {
-  const items = [
-    {
-      id: "doisdufajs34234",
-      label: "can i use react on a project",
-      content:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore doloribus qui quos nulla reprehenderit expedita deserunt quaerat vel. Tempore, odio? Rem optio ad non incidunt. Nemo voluptatum labore consequatur mollitia odio nostrum, voluptates ex, numquam possimus neque culpa, ab sed ipsam impedit atque incidunt consectetur exercitationem quod distinctio fugit officiis reiciendis hic pariatur voluptatem. Neque, sequi debitis quaerat reiciendis dolorem ratione corrupti corporis iusto iure praesentium consequatur veniam fugit facilis dignissimos aut tempore eum asperiores itaque atque recusandae tempora dicta cumque! Esse quibusdam iure ab quae! Aperiam cupiditate deleniti consequuntur eveniet repellat nulla, laborum sequi ex! Dolorem nisi obcaecati rerum?",
-    },
-    {
-      id: "doisduf4523452kl;jdfg34",
-      label: "can i use react on a project",
-      content:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nobis beatae placeat assumenda praesentium aut tempora dolorum, asperiores nesciunt doloremque autem magni, necessitatibus vitae atque quasi distinctio dolorem. Perferendis soluta deleniti culpa corrupti nesciunt doloremque consequatur quia eveniet repudiandae doloribus minus, impedit praesentium adipisci sunt recusandae libero facilis ipsa asperiores labore voluptates porro quam quidem! Ducimus culpa voluptatum atque laborum ratione nesciunt eos, porro itaque pariatur distinctio assumenda dolor deserunt veritatis similique sequi dignissimos quod numquam. Facilis itaque similique earum asperiores laboriosam exercitationem at delectus quo et qui magni harum laborum, neque quibusdam. Fugiat dolorem eos illum tenetur aut rerum.",
-    },
-    {
-      id: "doiasdfasdf43512345sdufajsdsfasdf34234",
-      label: "can i use react on a project",
-      content:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel praesentium quidem delectus asperiores quae cumque veniam optio culpa incidunt? Exercitationem officiis cupiditate pariatur impedit reiciendis aperiam itaque perferendis aliquid, placeat accusamus, sint nemo est, quibusdam qui fuga tenetur soluta commodi iusto. Aut eos maxime iste corporis, omnis quo, cum eum qui ullam sequi nobis enim accusantium doloribus deleniti totam error veniam hic, voluptatem debitis? Sint quam dolorem alias impedit quis quisquam, cupiditate officia inventore nostrum similique explicabo atque, recusandae iste saepe rem quae earum repellendus, doloribus quod consequatur perferendis quibusdam ducimus! Ipsam quidem non corporis quod. Quasi eum id reprehenderit?",
-    },
+  const options = [
+    { label: "Red", value: "red" },
+    { label: "Green", value: "green" },
+    { label: "Blue", value: "blue" },
   ];
-  return <Accordion items={items} />;
+  return <Dropdown options={options} />;
 }
 
 export default App;
---
+++ src/component/Dropdown.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+function Dropdown({ options }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const renderedOptions = options.map((option) => {
+    return <div key={option.value}>{option.label}</div>;
+  });
+  return (
+    <div>
+      <div>Select...</div>
+      <div>{renderedOptions}</div>
+    </div>
+  );
+}
+
+export default Dropdown;
