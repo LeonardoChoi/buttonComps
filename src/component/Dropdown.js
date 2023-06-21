ae1db3d1f53054c15a3378a3ba447cf74ec27084
unknown <leonardohchoi@gmail.com>
2023-06-21 15:18:06 -0300

    make handle click for dropdown

--- src/component/Dropdown.js
+++ src/component/Dropdown.js
@@ -3,13 +3,17 @@ import { useState } from "react";
 function Dropdown({ options }) {
   const [isOpen, setIsOpen] = useState(false);
 
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
   const renderedOptions = options.map((option) => {
     return <div key={option.value}>{option.label}</div>;
   });
   return (
     <div>
-      <div>Select...</div>
-      <div>{renderedOptions}</div>
+      <div onClick={handleClick}>Select...</div>
+      {isOpen && <div>{renderedOptions}</div>}
     </div>
   );
 }
