b5b4af5d2111203587c2df6567a983cd9c96f9be
unknown <leonardohchoi@gmail.com>
2023-06-22 13:27:36 -0300

    add state for term option

--- src/component/Dropdown.js
+++ src/component/Dropdown.js
@@ -1,14 +1,23 @@
 import { useState } from "react";
 
-function Dropdown({ options }) {
+function Dropdown({ options, selection, onSelect }) {
   const [isOpen, setIsOpen] = useState(false);
 
   const handleClick = () => {
     setIsOpen(!isOpen);
   };
 
+  const handleOptionClick = (option) => {
+    setIsOpen(false);
+    onSelect(option);
+  };
+
   const renderedOptions = options.map((option) => {
-    return <div key={option.value}>{option.label}</div>;
+    return (
+      <div onClick={() => handleOptionClick(option)} key={option.value}>
+        {option.label}
+      </div>
+    );
   });
   return (
     <div>
